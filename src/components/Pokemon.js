4e360319b2808ddd9b4864fb0ca083d413a15461
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-12-12 15:02:07 +0000

    Added background image

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,24 +5,23 @@ const Pokemon = (props) => {
   if (props.pokemon.length === 0) return null;
 
   const altTag = "Picture of " + props.pokemon[0];
-  const height = props.pokemon[2] / 10;
-  const weight = props.pokemon[3] / 10;
+  const height = props.pokemon[1] / 10;
+  const weight = props.pokemon[2] / 10;
 
-  const types = props.pokemon[5].map((element, index) => {
+  const types = props.pokemon[4].map((element, index) => {
     return <li key={index}>{element.type.name}</li>
   });
 
-  const moves = props.pokemon[6].map((element, index) => {
+  const moves = props.pokemon[5].map((element, index) => {
     return <li key={index}>{element.move.name}</li>
   });
 
   return(
     <div className="pokemon-details">
       <h3>{props.pokemon[0]}</h3>
-      <p>Pokedex Entry: {props.pokemon[1]}</p>
       <p>Height: {height}m</p>
       <p>Weight: {weight}kg</p>
-      <img src={props.pokemon[4]} alt={altTag}/>
+      <img src={props.pokemon[3]} alt={altTag} className="pokemon-image"/>
       <p>Type:</p>
         <ul>{types}</ul>
       <p>Moves:</p>
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = (props) => {
   return(
-    <h1>Original 151 Pokemon</h1>
+    <h1 className="title">Original 151 Pokemon</h1>
   );
 }
 
